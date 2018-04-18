6c76bb4e67602ad47c180486f8458bb87cd703f8
aleksandrandreichenko <rsn_alex@mail.ru>
2018-04-18 15:13:35 +0300

    added some dependencies

---
+++ src/main/java/org/frei/springboot/students/university/UniversityApplication.java
@@ -0,0 +1,21 @@
+package org.frei.springboot.students.university;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Base Application
+ *
+ *
+ * by Andreichenko
+ */
+
+@SpringBootApplication
+public class UniversityApplication {
+
+    public static void main(String[] args)throws Exception {
+        SpringApplication.run(UniversityApplication.class, args);
+    }
+}
