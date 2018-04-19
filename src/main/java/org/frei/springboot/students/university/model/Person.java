e1c50721cd51007ed17659ff6a436f4db88ed6f1
aleksandrandreichenko <rsn_alex@mail.ru>
2018-04-19 16:21:09 +0300

    Added Controller and Entities

--- src/main/java/org/frei/springboot/students/university/model/Person.java
+++ src/main/java/org/frei/springboot/students/university/model/Person.java
@@ -1,5 +1,27 @@
 package org.frei.springboot.students.university.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
-public class Person {
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person extends BaseEntity{
+
+    @Column(name = "first_name")
+    @NotEmpty
+    @NonNull
+    private String firstName;
+
+    @Column(name = "last-name")
+    @NotEmpty
+    @NonNull
+    private String lastName;
 }
