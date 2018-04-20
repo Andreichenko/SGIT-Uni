9e6b37642c5d8625de2895b92399ca37cc868b96
aleksandrandreichenko <rsn_alex@mail.ru>
2018-04-20 21:09:43 +0300

    update students

--- src/main/java/org/frei/springboot/students/university/students/Students.java
+++ src/main/java/org/frei/springboot/students/university/students/Students.java
@@ -2,10 +2,22 @@ package org.frei.springboot.students.university.students;
 
 
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
 
 @XmlRootElement
 public class Students {
 
+    private List<Student> students;
 
+    @XmlElement
+    public List<Student> getStudents() {
+        if (students == null){
+            students = new ArrayList<>();
+        }
+
+        return students;
+    }
 }
