58c9a4cc06d480dee6d37c6316d917421e880f64
aleksandrandreichenko <rsn_alex@mail.ru>
2018-04-21 19:43:20 +0300

    fix

--- src/main/java/org/frei/springboot/students/university/students/StudentController.java
+++ src/main/java/org/frei/springboot/students/university/students/StudentController.java
@@ -4,6 +4,7 @@ package org.frei.springboot.students.university.students;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
@@ -18,7 +19,13 @@ public class StudentController {
     }
 
     @GetMapping("/students.html")
-    public String showStudentList(Map<String, Object> model){
+    public Students showStudentList(Map<String, Object> model){
+        Students students = new Students();
+        students.getStudents().addAll(this.studentRepository.findAll());
+        return students;
+    }
+    @GetMapping({"/students.json", "/students.xml"})
+    public @ResponseBody Students showResourcesStudentList(){
         Students students = new Students();
         students.getStudents().addAll(this.studentRepository.findAll());
         return students;
--- src/main/java/org/frei/springboot/students/university/students/StudentRepository.java
+++ src/main/java/org/frei/springboot/students/university/students/StudentRepository.java
@@ -3,11 +3,12 @@ package org.frei.springboot.students.university.students;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
-public interface StudentRepository {
+public interface StudentRepository{
 
     @Transactional(readOnly = true)
     @Cacheable("students")
