509956f277dddf9e498b6450fddcf81adbc9bea4
aleksandrandreichenko <rsn_alex@mail.ru>
2018-05-01 00:24:43 +0300

    Preparning dates for tables in DB

--- src/main/java/org/frei/springboot/students/university/parents/Student.java
+++ src/main/java/org/frei/springboot/students/university/parents/Student.java
@@ -2,17 +2,29 @@ package org.frei.springboot.students.university.parents;
 
 
 import org.frei.springboot.students.university.model.NamedEntity;
+import org.frei.springboot.students.university.visit.Visit;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "students")
 public class Student extends NamedEntity {
 
-    private Date birthDay;   // birthday
+    @ManyToOne
+    @JoinColumn(name = "birth_date")
+    private Date birthDay;
+
+    @ManyToOne
     private StudentType studentType;
+
+
+    private Parent parent;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "studentId", fetch = FetchType.EAGER)
+    private Set<Visit> visits = new LinkedHashSet<>();
 
 
     public void isNew(){
