f035bd5fe56586c5d975e7e9bde88858b4aad422
aleksandrandreichenko <rsn_alex@mail.ru>
2018-05-01 21:46:53 +0300

    fix

--- src/main/java/org/frei/springboot/students/university/parents/Student.java
+++ src/main/java/org/frei/springboot/students/university/parents/Student.java
@@ -3,11 +3,12 @@ package org.frei.springboot.students.university.parents;
 
 import org.frei.springboot.students.university.model.NamedEntity;
 import org.frei.springboot.students.university.visit.Visit;
+import org.springframework.beans.support.MutableSortDefinition;
+import org.springframework.beans.support.PropertyComparator;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import java.util.Date;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table(name = "students")
@@ -15,19 +16,53 @@ public class Student extends NamedEntity {
 
     @ManyToOne
     @JoinColumn(name = "birth_date")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date birthDay;
 
     @ManyToOne
+    @JoinColumn(name = "student_type")
     private StudentType studentType;
 
-
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
     private Parent parent;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "studentId", fetch = FetchType.EAGER)
     private Set<Visit> visits = new LinkedHashSet<>();
 
+    public Date getBirthDay() {
+        return this.birthDay;
+    }
 
-    public void isNew(){
+    public StudentType getStudentType() {
+        return this.studentType;
+    }
 
+    public Parent getParent() {
+        return this.parent;
+    }
+
+    protected Set<Visit> getVisitsInternal() {
+        if (this.visits == null) {
+            this.visits = new HashSet<>();
+        }
+        return this.visits;
+    }
+
+    protected void setVisitsInternal(Set<Visit> visits) {
+        this.visits = visits;
+    }
+
+    public List<Visit> getVisits() {
+        List<Visit> sortedVisits = new ArrayList<>(getVisitsInternal());
+        PropertyComparator.sort(sortedVisits,
+                new MutableSortDefinition("date", false, false));
+        return Collections.unmodifiableList(sortedVisits);
+    }
+
+    public void addVisit(Visit visit) {
+        getVisitsInternal().add(visit);
+        visit.setStudentId(this.getId());
     }
 }
