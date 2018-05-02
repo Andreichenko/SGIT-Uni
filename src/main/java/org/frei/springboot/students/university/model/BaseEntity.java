a26e4b1971694ddc02105299c0d1ecc5b44e595e
aleksandrandreichenko <rsn_alex@mail.ru>
2018-05-02 21:04:10 +0300

    fix

--- src/main/java/org/frei/springboot/students/university/model/BaseEntity.java
+++ src/main/java/org/frei/springboot/students/university/model/BaseEntity.java
@@ -6,16 +6,27 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 @MappedSuperclass
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+
+    public boolean isNew(){
+        return this.id == null;
+    }
 }
